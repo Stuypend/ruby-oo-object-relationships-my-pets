6b521b416ce4dc1dd5514c7169971e526d94ee14
stuypend <andre@stuypend.com>
2021-02-27 18:30:38 -0500

    commit tree b5922fd80a2ed93436a9f8e9c1d2e9803247d097

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,92 @@
 class Owner
-  # code goes here
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name, :species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.count
+    return @@all.size
+  end
+
+  def self.reset
+    return @@all.clear
+  end
+
+  def cats
+    Cat.all.select do
+      |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do
+    |dog|
+      dog.owner == self
+    end
+  end
+
+    def walk_dogs
+      Dog.all.each do
+        |dog|
+        dog.mood = "happy"
+      end
+    end
+
+      def feed_cats
+        Cat.all.each do
+        |dog|
+          dog.mood = "happy"
+        end
+      end
+
+      def sell_pets
+        Cat.all.each do
+        |dog|
+          dog.mood = "nervous"
+          dog.owner = nil
+        end
+
+        Dog.all.each do
+        |dog|
+          dog.mood = "nervous"
+          dog.owner = nil
+        end
+      end
+
+      def list_pets
+        ("I have #{self.dogs.count} dog(s), and #{cats.count} cat(s).")
+      end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    puts "I am a human."
+    return "I am a human."
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
 end
